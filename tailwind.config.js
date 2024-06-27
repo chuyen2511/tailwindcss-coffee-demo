8d19d4bc0a9b7d0d4c648df79d4057982d865bb5
chuyentran2511 <tchuyen23@gmail.com>
2024-06-28 02:45:10 +0800

    update FEATURED MUGS

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,26 @@ module.exports = {
         Karla:['Karla', 'sans-serif']
       },
       colors:{
-        'light-coffee':'#C89F94'
+        'light-coffee':'#C89F94',
+        'coffee':{
+          50: 'E8D6D0',
+          200:'#A25F4B',
+          600:'#744838',
+        }
       },
       keyframes: {
         slideDown: {
           '0%': { transform: 'translateY(-100%)' },
           '100%': { transform: 'translateY(0%)' },
-        }
+        },
+        fadeIn:{
+          from : {opacity:0}, to:{ opacity:1}
+        },
       },
       animation: {
         slideDown: 'slideDown .3s ease-in-out',
+        fadeIn: 'fadeIn .5s ease-in-out',
+
       }
     },
   },
